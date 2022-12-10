fd776b2c675dab1f1568088114db18d7855ad5c4
Lachermeier <tobias.lachermeier@inovex.de>
2022-12-11 00:53:47 +0100

    add Day9

---
+++ AoC22/Day9/Day9.swift
@@ -0,0 +1,20 @@
+//
+//  Day9.swift
+//  AoC22
+//
+//  Created by Tobias Lachermeier on 11.12.22.
+//
+
+import Foundation
+
+class Day9: Day {
+    var day: Int = 9
+    
+    func solveFirstQuiz() {
+        print("")
+    }
+    
+    func solveSecondQuiz() {
+        print("")
+    }
+}
